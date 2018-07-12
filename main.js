a15aadd10844351f2e47c18f75b99a3ecab93c1f
Daniel Luty <your_email@example.com>
2018-07-12 07:21:45 +0200

    current version

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
-                scrollTop: target.offset().top + 22
+                scrollTop: target.offset().top - 100
             }, 600);
         }
     });
@@ -24,27 +24,29 @@ $(document).ready(function () {
     $(window).scroll(function () {
         if ($(document).scrollTop() > 790) {
 
-            $('.nav').css({'height': '4rem','padding': '.4rem'});
+            $('.nav').css({'height': '4rem','padding': '.4rem','align-items': 'center'});
 
-            $('.logo').css({'border-radius': '.3rem', 'padding': '.5rem 1rem 1rem'});
+            $('.logo').css({'border-radius': '.3rem', 'padding': '.7rem 1rem 1rem'});
 
             $('.logoText').text('!Avoid!').
-            css({'left': '3rem', 'letter-spacing': '.50rem', 'padding-bottom': '2px solid rgba(0, 0, 0, 0.6)'});
+            css({'left': '3.4rem', 'bottom': '.5rem', 'letter-spacing': '.50rem', 'padding-bottom': '2px solid rgba(0, 0, 0, 0.6)'});
 
-            $('.flagDE').css('border-radius', '0 .7rem 0 .7rem'); $('.flagAT').css('border-radius', '.7rem 0 .7rem 0');
+            $('.flagDE').css({'border-radius': '0 .7rem 0 .7rem', 'transform': 'rotate(35deg)'});
+            $('.flagAT').css({'border-radius': '.7rem 0 .7rem 0', 'transform': 'rotate(-35deg)'});
 
             $('.flag').css({'width': '35', 'height': '24'});
 
         } else {
 
-            $('.nav').css({'height': '7rem','padding': '1rem'});
+            $('.nav').css({'height': '7rem','padding': '.8rem','align-items': 'flex-end'});
 
-            $('.logo').css({'border-radius': '.8rem .8rem .2rem .2rem', 'padding-top': '.4rem'});
+            $('.logo').css({'border-radius': '.8rem .8rem 0 0', 'padding-top': '.4rem'});
 
             $('.logoText').html('CALL<br>avoidance!').
-            css({'left': '.6rem', 'letter-spacing': '.55rem', 'text-decoration': 'none', 'border-bottom': 'none'});
+            css({'left': '1.2rem', 'bottom': '.4rem', 'letter-spacing': '.55rem', 'text-decoration': 'none', 'border-bottom': 'none'});
 
-            $('.flagDE').css('border-radius', '.7rem 0 .7rem 0'); $('.flagAT').css('border-radius', '0 .7rem 0 .7rem');
+            $('.flagDE').css({'border-radius': '.7rem 0 .7rem 0', 'transform': 'rotate(0)'});
+            $('.flagAT').css({'border-radius': '0 .7rem 0 .7rem', 'transform': 'rotate(0)'});
 
             $('.flag').css({'width': '53','height': '37'});
 
@@ -54,8 +56,8 @@ $(document).ready(function () {
     /* ***************************************** SHOW - HIDE *** */
 
     $('.avoidanceBoxToggler').on('click', function () {
+        $('.avoidanceBox').not($(this).next()).slideUp();
         $(this).next().slideToggle(function () {
-
         });
     });
 
